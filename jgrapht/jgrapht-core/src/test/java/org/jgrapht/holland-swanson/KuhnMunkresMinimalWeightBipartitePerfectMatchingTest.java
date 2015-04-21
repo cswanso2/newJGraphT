3f9e4fcccaa412e0ea0c8257011faaed1a804298
Brandon Holland <beholla2@illinois.edu>
2015-04-21 14:36:11 -0500

    Test

--- jgrapht/jgrapht-core/src/test/java/org/jgrapht/holland-swanson/KuhnMunkresMinimalWeightBipartitePerfectMatchingTest.java
+++ jgrapht/jgrapht-core/src/test/java/org/jgrapht/holland-swanson/KuhnMunkresMinimalWeightBipartitePerfectMatchingTest.java
@@ -18,6 +18,7 @@ public class KuhnMunkresMinimalWeightBipartitePerfectMatchingTest {
 
         List<String> list0 = new LinkedList<String>();
         List<String> list1 = new LinkedList<String>();
+
         assertEquals(Collections.emptySet(), new KuhnMunkresMinimalWeightBipartitePerfectMatching<String, DefaultWeightedEdge>(graph, list0, list1).getMatching());
     }
 }
