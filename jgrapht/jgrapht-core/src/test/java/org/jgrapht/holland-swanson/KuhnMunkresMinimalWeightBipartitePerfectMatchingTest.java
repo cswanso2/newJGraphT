5b0b19d5c11511c88f5ebf68ec2e0915ba8f1bbe
Brandon Holland <beholla2@illinois.edu>
2015-04-21 14:24:46 -0500

    Edits

--- jgrapht/jgrapht-core/src/test/java/org/jgrapht/holland-swanson/KuhnMunkresMinimalWeightBipartitePerfectMatchingTest.java
+++ jgrapht/jgrapht-core/src/test/java/org/jgrapht/holland-swanson/KuhnMunkresMinimalWeightBipartitePerfectMatchingTest.java
@@ -15,7 +15,7 @@ public class KuhnMunkresMinimalWeightBipartitePerfectMatchingTest {
 
     @Test
     public void EmptyClass() {
-        DirectedGraph<String, DefaultEdge> graph = new SimpleDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+
         List<String> list0 = new LinkedList<String>();
         List<String> list1 = new LinkedList<String>();
         assertEquals(Collections.emptySet(), new KuhnMunkresMinimalWeightBipartitePerfectMatching<String, DefaultWeightedEdge>(graph, list0, list1).getMatching());
