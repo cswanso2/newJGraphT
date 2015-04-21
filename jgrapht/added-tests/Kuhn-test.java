b9ec3b92f86738d3d7a2076138e2613f6898166b
Brandon Holland <beholla2@illinois.edu>
2015-04-21 13:52:03 -0500

    Added Kuhn Test

---
+++ jgrapht/added-tests/Kuhn-test.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.After;
+package org.jgrapht.graph;
+import java.util.*;
+import org.jgrapht.*;
+
+public class KuhnMunkresMinimalWeightBipartitePerfectMatchingTest {
+    // Derive an Edge Coverage test set T_E for checkItExpand such
+    // that T_E does not satisfy GACC on the predicate in checkIt.
+    // [TODO] Write your test set for T_E in JUnit.
+
+    @Test
+    public void EmptyClass() {
+        
+        assertEquals(Collections.emptySet(), new KuhnMunkresMinimalWeightBipartitePerfectMatching<VertexType, DefaultWeightedEdge>(graph, list0, list1).getMatching(););
+    }
+}
