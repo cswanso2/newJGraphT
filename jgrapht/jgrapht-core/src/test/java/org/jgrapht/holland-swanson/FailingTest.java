07bb34efeef0b2fcd0c129c758c76d130ea7ba42
Charles Swanson <cswanso2@illinois.edu>
2015-04-21 21:32:06 -0500

    a

--- jgrapht/jgrapht-core/src/test/java/org/jgrapht/holland-swanson/FailingTest.java
+++ jgrapht/jgrapht-core/src/test/java/org/jgrapht/holland-swanson/FailingTest.java
@@ -4,7 +4,8 @@ package org.jgrapht.graph;
 
 import org.jgrapht.*;
 import org.junit.Test;
-import static org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.*;
