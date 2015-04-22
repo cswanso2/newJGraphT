b0254949786973ccdf87d9611a863619cf16b2de
Charles Swanson <cswanso2@illinois.edu>
2015-04-21 19:41:05 -0500

    new tests

--- jgrapht/jgrapht-core/src/test/java/org/jgrapht/holland-swanson/SimpleGraphTest.java
+++ jgrapht/jgrapht-core/src/test/java/org/jgrapht/holland-swanson/SimpleGraphTest.java
@@ -68,4 +68,85 @@ public class SimpleGraphTest
         assertEquals(false, g.hasEdge(vertex2, vertex1));
 	}	
 
+	@Test
+	public void testRemoveVertex()
+	{
+		DirectedGraph<String, Integer> g = new SimpleDirectedGraph<String, Integer>(Integer.class);
+		String vertex1 = "1";
+        g.addVertex(vertex1);
+        String vertex2 = "2";
+        g.addVertex(vertex2);
+        assertEquals(2, g.vertexSet().size() );
+  		g.removeVertex(vertex1);
+  		g.removeVertex(vertex2);
+        assertEquals(0, g.vertexSet().size() );
+	}
+
+	@Test
+	public void testRemoveVertex()
+	{
+		DirectedGraph<String, Integer> g = new SimpleDirectedGraph<String, Integer>(Integer.class);
+		String vertex1 = "1";
+        g.addVertex(vertex1);
+        String vertex2 = "2";
+        g.addVertex(vertex2);
+        assertEquals(2, g.vertexSet().size() );
+  		g.removeVertex(vertex1);
+  		g.removeVertex(vertex2);
+        assertEquals(0, g.vertexSet().size() );
+	}
+
+	@Test
+	public void removeEdge()
+	{
+		UndirectedGraph<String, Integer> g = new SimpleGraph<String, Integer>(Integer.class);
+		String vertex1 = "1";
+        g.addVertex(vertex1);
+        String vertex2 = "2";
+        g.addVertex(vertex2);
+        g.addEdge(vertex1, vertex2);
+        g.removeEdge(vertex1, vertex2);
+        assertEquals(false, g.hasEdge(vertex1, vertex2));
+	}
+
+	@Test
+	public void removeVertexRemovesEdge()
+	{
+		UndirectedGraph<String, Integer> g = new SimpleGraph<String, Integer>(Integer.class);
+		String vertex1 = "1";
+        g.addVertex(vertex1);
+        String vertex2 = "2";
+        g.addVertex(vertex2);
+        g.addEdge(vertex, vertex2);
+  		g.removeVertex(vertex1);
+        assertEquals(false, g.hasEdge(vertex1, vertex2) );
+	}
+
+	@Test
+	public void degreeOf()
+	{
+		UndirectedGraph<String, Integer> g = new SimpleGraph<String, Integer>(Integer.class);
+		String vertex1 = "1";
+        g.addVertex(vertex1);
+        assertEquals(0, g.degreeOf(vertex1));
+        String vertex2 = "2";
+        g.addVertex(vertex2);
+        g.addEdge(vertex1, vertex2);
+        assertEquals(1,  g.degreeOf(vertex1));
+        g.removeVertex(vertex2);
+        assertEquals(0, g.degreeOf(vertex1));
+	}
+
+	@Test
+	public void containsVertex()
+	{
+		DirectedGraph<String, Integer> g = new SimpleDirectedGraph<String, Integer>(Integer.class);
+		String vertex1 = "1";
+        g.addVertex(vertex1);
+        String vertex2 = "2";
+        g.addVertex(vertex2);
+        String vertex3 = "3";
+        assertEquals(true, g.containsVertex(vertex1));
+        assertEquals(false, g.containsVertex(vertex3));
+	}
 }
