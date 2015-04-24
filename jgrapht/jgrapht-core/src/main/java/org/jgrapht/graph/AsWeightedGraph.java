c80c20cdd5e346931b5a76a1cf858fb89f191c11
Brandon Holland <beholla2@illinois.edu>
2015-04-23 20:32:15 -0500

    added functionality

--- jgrapht/jgrapht-core/src/main/java/org/jgrapht/graph/AsWeightedGraph.java
+++ jgrapht/jgrapht-core/src/main/java/org/jgrapht/graph/AsWeightedGraph.java
@@ -140,6 +140,43 @@ public class AsWeightedGraph<V, E>
 
         return weight;
     }
+
+    // Added by Brandon Holland
+    /**
+     * @see Graph#getEdgeWeight
+     */
+    @Override public int getEdgeWeight(E e)
+    {
+        int weight;
+
+        // Always return the value from the weight map first and
+        // only pass the call through as a backup
+        if (weightMap.containsKey(e)) {
+            weight = weightMap.get(e);
+        } else {
+            weight = super.getEdgeWeight(e);
+        }
+
+        return weight;
+    }
+
+    /**
+     * @see Graph#getEdgeWeight
+     */
+    @Override public boolean getEdgeWeight(E e)
+    {
+        boolean weight;
+
+        // Always return the value from the weight map first and
+        // only pass the call through as a backup
+        if (weightMap.containsKey(e)) {
+            weight = weightMap.get(e);
+        } else {
+            weight = super.getEdgeWeight(e);
+        }
+
+        return weight;
+    }
 }
 
 // End AsWeightedGraph.java
